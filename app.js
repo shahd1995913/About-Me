858ea5fd8865e176fbb3f11822e439dd079fa06d
shahd1995913 <shahdalkhatib95@gmail.com>
2021-06-21 16:18:22 +0300

    update2

---
+++ app.js
@@ -0,0 +1,71 @@
+
+function informationAboutMe1(){
+    alert ( "welecom to my page , please chose Yas/No Answer in the following Qustion ??" )
+    
+    let answer = prompt ("Is my name Shahd?")
+    
+    if(answer == "Yas"){alert ("Correct.")}
+    
+    else if(answer == "No"){alert ("Not Correct My name is shahd.")}
+
+}
+
+
+
+function informationAboutMe2(){
+    alert ( "The second Qustion ? please chose Yas/No Answer in the following Qustion ??" )
+    
+    let answer1 = prompt ("Did I study computer science?")
+    
+    if(answer1 == "Yas"){alert ("Correct.")}
+    
+    else if(answer1 == "No"){alert ("Not Correct , I study computer science.")}
+
+}
+
+
+
+function informationAboutMe3(){
+    alert ( "The Thired Qustion ? please chose Yas/No Answer in the following Qustion ??" )
+    
+    let answer3 = prompt ("Did I graduate in 2017 from university")
+    
+    if(answer3 == "Yas"){alert ("Correct.")}
+    
+    else if(answer3 == "No"){alert ("Not Correct , I graduate in 2017 from university.")}
+
+}
+
+function informationAboutMe4(){
+    alert ( "The forth Qustion ? please chose Yas/No Answer in the following Qustion ??" )
+    
+    let answer4 = prompt ("Do I know about artificial intelligence?")
+    
+    if(answer4 == "Yas"){alert ("Correct.")}
+    
+    else if(answer4 == "No"){alert ("Not Correct , I study the  artificial intelligence?.")}
+
+}
+
+
+function informationAboutMe5(){
+    alert ( "The fiveth Qustion ? please chose Yas/No Answer in the following Qustion ??" )
+    
+    let answer5 = prompt ("Have I ever trained in programming?")
+    
+    if(answer5 == "Yas"){alert ("Correct.")}
+    
+    else if(answer5 == "No"){alert ("Not Correct , I trained in programming.")}
+
+}
+
+
+    informationAboutMe1()
+    informationAboutMe2()
+    informationAboutMe3()
+    informationAboutMe4()
+    informationAboutMe5()
+  
+
+
+
